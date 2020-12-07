7028bf197f2c56ca044088793ab4593f7897fd53
Erika Segerström <erikasegerstrom@gmail.com>
2020-12-07 18:35:50 +0100

    add first two puzzles

---
+++ adventofcode_1_1.py
@@ -0,0 +1,35 @@
+def import_data():
+    text_file = open("adventofcode_1_1_input.txt", "r")
+    lines = text_file.read().split('\n')
+    return lines
+
+input = import_data()
+
+input = list(map(int, input))
+
+def run_1_1(input):
+    """
+    First puzzle
+    """
+    for nbr in input:
+        residual = 2020 - nbr
+        if residual in input:
+            print('Solution: ', residual * nbr)
+
+
+def run_1_2(input):
+    """
+    Second puzzle
+    """
+    length  = range(len(input))
+    for i in length:
+        j = i + 1
+        while j < len(input):
+            residual = 2020 - (input[i] + input[j])
+            if residual in input:
+                print('Solution: ', input[i]*input[j]*residual)
+                exit()
+            j += 1
+
+#run_1_1(input)
+#run_1_2(input)
---
+++ adventofcode_2_1.py
@@ -0,0 +1,29 @@
+import re
+
+def import_data():
+    text_file = open("adventofcode_2_1_input.txt", "r")
+    lines = text_file.read().split('\n')
+    return lines
+
+input = import_data()
+
+# First puzzle
+n_correct = 0
+for element in input:
+    parsed = re.split('[: -]', element)
+    n_occurance = parsed[4].count(parsed[2])
+    if int(parsed[0]) <= n_occurance <= int(parsed[1]):
+        n_correct += 1
+
+print('Number of correct pwds: ', n_correct)
+
+# Second puzzle
+n_correct = 0
+for element in input:
+    parsed = re.split('[: -]', element)
+    char = parsed[2]
+    first = parsed[4][int(parsed[0])-1]
+    second = parsed[4][int(parsed[1])-1]
+    if (first == char and second != char) or (first != char and second == char):
+        n_correct += 1
+print('Number of correct pwds: ', n_correct)
